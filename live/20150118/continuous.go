ee07285413fe7d90c75a0a27a66e8d46c9f139e5
vserpoul <vincent@serpoul.com>
2016-01-18 12:35:47 +0800

    better solution

--- live/20150118/continuous.go
+++ live/20150118/continuous.go
@@ -1,37 +1,24 @@
 package solution
 
-import "fmt"
-
 // Solution returns the longest binary gap
 func Solution(X []int, soughtSum int) bool {
 
 	sum := 0
-	potentialSum := []int{}
+	lower := 0
+	upper := 0
 
-	for _, val := range X {
-		sum += val
-		potentialSum = append(potentialSum, val)
-		fmt.Printf("appending %d, new sum == %d", val, sum)
+	for upper <= len(X) && lower < len(X) {
 
-		if sum > soughtSum {
-			for sum > soughtSum {
-				fmt.Printf(": sum too high, removing the first member, %d ", potentialSum[0])
-				sum -= potentialSum[0]
-				if len(potentialSum) == 1 {
-					potentialSum = []int{}
-				} else {
-					potentialSum = potentialSum[1:len(potentialSum)]
-				}
-			}
+		if sum < soughtSum {
+			sum += X[upper]
+			upper++
+		} else if sum > soughtSum {
+			sum -= X[lower]
+			lower++
 		}
-
 		if sum == soughtSum {
-			fmt.Printf("all good, sum ==")
 			return true
 		}
-
-		fmt.Printf(" >> sum: %d, new table: %v\n", sum, potentialSum)
-
 	}
 
 	return false
